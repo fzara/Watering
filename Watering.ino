cb5064a8f72e48292a3e94ca54adcdbc94afde18
fzara <fzara@users.noreply.github.com>
2015-06-15 14:21:07 +0200

    Debugging

--- Watering.ino
+++ Watering.ino
@@ -4,17 +4,23 @@ This is a simple controller to allow programmable watering of plants
 
 */
 
+//libraries to include
+#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>  //To be included in future releases
+#include "RTClib.h"
+
 // Pin configuration
 const byte pump1Pin = 15;
+const byte pump1LedPin = 13;
 
 
-//pinMode declaration
-pinMode (pump1Pin, OUTPUT);
+
 
 
 void setup()
 {
-  
+ //pinMode declaration
+pinMode(pump1Pin, OUTPUT); 
 }
 
 void loop()
@@ -25,7 +31,7 @@ void loop()
 //Start watering function lasting specified minutes
 void DoWatering(int minutes)
 {
-	unsigned long milliseconds = minutes * 60000);
+	unsigned long milliseconds = (minutes * 60000);
 	unsigned long now_milliseconds = millis();
 	while (now_milliseconds + milliseconds < millis())
 		{
