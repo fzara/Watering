1c8d06abedf45a9636c4a989f5ca2ba5132a0e75
fzara <fzara@users.noreply.github.com>
2015-07-02 18:30:51 +0200

    Introducing new commander library
    Complete rewrite, using ArduinoSerialCommand

--- Watering.ino
+++ Watering.ino
@@ -2,7 +2,8 @@
 ### Arduino Watering System ###
 This is a simple controller to allow programmable watering of plants
 
-Commands available over serial/bluetooth (case-insensitive):
+Commands available over serial/bluetooth (case-sensitive):
+	# LEDTEST 	Makes the led blink 5 times (testing purposes)
 	# START		Enables the pump
 	# STOP		Disables the pump
 	# OVRON		Prevents the pump from starting
@@ -11,9 +12,10 @@ Commands available over serial/bluetooth (case-insensitive):
 */
 
 //libraries to include
-#include <Wire.h>
+#include <SerialCommand.h>
 //#include <LiquidCrystal_I2C.h>  //To be included in future releases
-#include "RTClib.h"
+//#include "RTClib.h" 	//To be included with Wire.h
+//#include <Wire.h>		//To be included with RTClib.h
 #include <SoftwareSerial.h>
 
 #define DEBUG_MODE
@@ -26,28 +28,32 @@ const byte pump1Pin = 13;
 const byte pump1LedPin = 13;
 
 // Var Declarations
-String inBuffer;
 int mins;
-String command;
 bool pumpIsActive, pumpOvrStop = 0;
-int num2, num3;
 
 //#ifdef DEBUG_MODE
 #define DB_RX_PIN 8
 #define DB_TX_PIN 9
 //#endif
 
+//SerialCommand object
+SerialCommand SCmd;
+
 void setup()
 {
  //pinMode declaration
 pinMode(pump1Pin, OUTPUT); 
 
-
 //Serials configurations
 
 //Serial.begin(115200); // Serial for ESP8266 Wi-Fi Module
-Serial.begin(9800); //Serial for HC-06 Bluetooth module
+Serial.begin(9800); //Serial for receiving commands, correct baud rate accordingly
 
+// Setup callbacks for SerialCommand commands 
+  SCmd.addCommand("LEDTEST",LED_test);   	// Tests the led with 5 rapid blinking
+  SCmd.addCommand("OVR",OvrSet);		// Enables/disables security override 
+  SCmd.addDefaultHandler(unrecognized);  	// Handler for command that isn't matched  (says "What?") 
+  Serial.println("READY"); 
 
 #ifdef DEBUG_MODE
 SoftwareSerial Serialdb(DB_RX_PIN,DB_TX_PIN); // RX,TX - Serial for debugging purposes
@@ -57,121 +63,41 @@ Serialdb.println("Debug Mode ON");
 }
 
 void loop()
-{
-																		// Main Pump Control part
-	if (pumpIsActive & !pumpOvrStop){
-		digitalWrite(pump1Pin, HIGH);
-		digitalWrite(pump1LedPin, HIGH);
-	}
-	else {
-		digitalWrite(pump1Pin, LOW);
-		digitalWrite(pump1LedPin, LOW);
-	}
-	
-																		// Serial data collection
-	if (Serial.available())
-	{
-		char c = Serial.read();
-		if (c == '\n')
-		{
-			parseCommand(inBuffer); 							// Call function to parse serial commands
-			inBuffer = "";
-		}
-		else
-		{
-			inBuffer += c;
-		}
-		
-	}
-	
+{												
+	SCmd.readSerial();     // Process Serial Commands
 }
 
-void parseCommand(String com)									// Function to parse serial commands
+void LED_test()
 {
-	String part1,part2,part3;
-	int nextSpace, lastSpace;
-	part1 = com.substring(0, com.indexOf(" "));
-	lastSpace = com.indexOf(" ");
-	nextSpace = com.indexOf(' ',lastSpace+1);
-	part2 = com.substring(lastSpace +1,nextSpace);
-	nextSpace = com.indexOf(' ',lastSpace+1);
-	lastSpace = nextSpace;
-	part3 = com.substring(lastSpace+1);
-	/*
-		Serial.print("Part1: \t"); 												//TESTING
-		Serial.println(part1);														//TESTING
-		Serial.print("Part2: \t");													//TESTING
-		Serial.println(part2);														//TESTING
-		Serial.print("Part3: \t");													//TESTING
-		Serial.println(part3);														//TESTING
-	*/
-		char part[10];					//convert to char array to elaborate command.
-		part1.toCharArray(part,10);
-		
-	if (strcasestr(part,"START"))		//START command
+  for (byte i=0; i<5; i++)
+  {
+	  digitalWrite(pump1LedPin, HIGH);
+	  delay(100);
+	  digitalWrite(pump1LedPin, LOW);
+	  delay(100);
+  }
+}
+
+void OvrSet()
+{
+	char *arg;
+	int setting;
+	arg = SCmd.next();
+	if (arg)
 	{
-		num2 = part2.toInt();
-		//num3 = part3.toInt();    	//part3 not used yet
-	/*
-		Serial.print("Num2: \t");													//TESTING
-		Serial.println(num2);														//TESTING
-		Serial.print("Num3: \t");													//TESTING
-		Serial.println(num3);														//TESTING
-	*/
-		if (num2 > 0)
+		setting = atoi(arg);
+		if (setting == 1)
 		{
-			Serial.print("Minutes SET to:");										//TESTING
-			Serial.println(num2);													//TESTING
+			pumpOvrStop = 1;
 		}
-		pumpIsActive = 1;
-	}
-	else if (strcasestr(part,"STOP"))		//STOP command
-	{
-	//	Serial.println("STOP RECEIVED");		//TESTING
-		pumpIsActive = 0;
-	}
-	else if (strcasestr(part,"OVRON"))		//OVRSTOPON command
-	{
-	//	Serial.println("OVRON RECEIVED");		//TESTING
-		pumpOvrStop = 1;
-	}
-	else if (strcasestr(part,"OVROFF"))		//OVRSTOPOFF command
-	{
-	//	Serial.println("OVROFF RECEIVED");		//TESTING
-		pumpOvrStop = 0;
+		else if (setting == 0)
+		{
+			pumpOvrStop = 0;
+		}
 	}
 }
 
-//Starts watering function lasting specified minutes
-void DoWatering(int minutes)
+void unrecognized()
 {
-	unsigned long milliseconds = (minutes * 60000);
-	unsigned long now_milliseconds = millis();
-		Serial.print("Starting DoWatering() \t"); 							//TESTING
-		Serial.print("now_milliseconds = "); 									//TESTING
-		Serial.println(now_milliseconds);										//TESTING
-		Serial.print("milliseconds to water = "); 							//TESTING
-		Serial.println(milliseconds);												//TESTING
-		Serial.print("milliseconds Sum = "); 									//TESTING
-		Serial.println(milliseconds+now_milliseconds);						//TESTING
-	while (millis() < now_milliseconds + milliseconds)
-		{
-			digitalWrite(pump1Pin, HIGH);
-			digitalWrite(pump1LedPin, HIGH);
-		}
-	digitalWrite(pump1Pin, LOW);
-	digitalWrite(pump1LedPin, LOW);
-	
-	
-}
-
-void StopWatering(int minutes)
-{
-	unsigned long milliseconds = (minutes * 60000);
-	unsigned long now_milliseconds = millis();
-	while (now_milliseconds + milliseconds < millis())
-		{		}
-	digitalWrite(pump1Pin, LOW);
-	digitalWrite(pump1LedPin, LOW);
-	
+  Serial.println("What?"); 
 }
