b24a143060fc9b1ed78bac55c1db39b553d1d424
fzara <fzara@users.noreply.github.com>
2015-07-03 18:13:25 +0200

    Added functions
    START (timed and not), STOP, OVR

--- Watering.ino
+++ Watering.ino
@@ -23,12 +23,14 @@ Commands available over serial/bluetooth (case-sensitive):
 #endif
 
 // Pin configuration
-const byte pump1Pin = 13;
+const byte pump1Pin = 2; //Has to be a PWM capable pin
 const byte pump1LedPin = 13;
 
 // Var Declarations
-int mins;
-bool pumpIsActive, pumpOvrStop = 0;
+int minsToWater,pump1Speed;
+bool pumpIsActive, pumpOvrStop, minsWatering = 0;
+unsigned long prevMillis;
+
 
 //#ifdef DEBUG_MODE
 #define DB_RX_PIN 8
@@ -41,7 +43,11 @@ SerialCommand SCmd;
 void setup()
 {
  //pinMode declaration
-pinMode(pump1Pin, OUTPUT); 
+pinMode(pump1Pin, OUTPUT);
+pinMode(pump1LedPin, OUTPUT); 
+
+//Stardard pump speed
+pump1Speed = 255;
 
 //Serials configurations
 
@@ -50,8 +56,10 @@ Serial.begin(9800); //Serial for receiving commands, correct baud rate according
 
 // Setup callbacks for SerialCommand commands 
   SCmd.addCommand("LEDTEST",LED_test);   	// Tests the led with 5 rapid blinking
-  SCmd.addCommand("OVR",OvrSet);		// Enables/disables security override 
-  SCmd.addDefaultHandler(unrecognized);  	// Handler for command that isn't matched  (says "What?") 
+  SCmd.addCommand("OVR",OvrSet);				// Enables/disables security override
+  SCmd.addCommand("START",StartWatering);	// Starts watering for a given time, or indefinitely
+  SCmd.addCommand("STOP",StopWatering);	// Stops watering
+  SCmd.addDefaultHandler(unrecognized);  	// Handler for command that isn't matched  (says "INVALIDCOMMAND") 
   Serial.println("READY"); 
 
 #ifdef DEBUG_MODE
@@ -64,6 +72,34 @@ Serialdb.println("Debug Mode ON");
 void loop()
 {												
 	SCmd.readSerial();     // Process Serial Commands
+	
+	//Main Watering control
+	
+	if (!pumpOvrStop && pumpIsActive)
+	{
+		analogWrite(pump1Pin, pump1Speed);
+		digitalWrite(pump1LedPin, HIGH);
+	}
+	else if (!pumpIsActive)
+	{
+		analogWrite(pump1Pin, 0);
+		digitalWrite(pump1LedPin, LOW);
+	}
+	else if (pumpOvrStop) 
+	{
+		analogWrite(pump1Pin, 0);
+		digitalWrite(pump1LedPin, LOW);
+	}
+	
+	if (minsWatering)
+	{
+		if (millis() < (prevMillis + (minsToWater)*60000))
+		{
+			pumpIsActive = 1;
+		}
+		else pumpIsActive = 0;
+	}
+	
 }
 
 void LED_test()
@@ -88,6 +124,7 @@ void OvrSet()
 		if (setting == 1)
 		{
 			pumpOvrStop = 1;
+			Serial.println("OVR = 1");
 		}
 		else if (setting == 0)
 		{
@@ -96,7 +133,31 @@ void OvrSet()
 	}
 }
 
+void StartWatering()
+{
+	char *arg;
+	arg = SCmd.next();
+	if (arg != NULL)
+	{
+		minsToWater = atoi(arg);
+		minsWatering = 1;
+		prevMillis = millis();
+		Serial.print("minsWatering = true, Minutes to water: ");
+		Serial.println(minsToWater);
+	}
+	else 
+	{
+		minsWatering = 0;
+	}
+	pumpIsActive = 1;
+}
+
+void StopWatering()
+{
+	pumpIsActive = 0;
+	minsWatering = 0;
+}
 void unrecognized()
 {
-  Serial.println("What?"); 
+  Serial.println("INVALIDCOMMAND"); 
 }
